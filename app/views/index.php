6d3d9f813c862e7b64095f659d5ace2e0d77e961
machula.remigiusz <machula.remigiusz@gmail.com>
2019-07-24 17:00:31 +0200

    correction. cinema hours display

--- app/views/index.php
+++ app/views/index.php
@@ -2,57 +2,59 @@
 
 
 
-    <table class="table table-striped table-dark" style="color: #cdcdcd">
-        <thead></thead>
-        <tbody>
-        <?php
+<table class="table table-striped table-dark" style="color: #cdcdcd">
+    <thead></thead>
+    <tbody>
+    <?php
 
-        foreach ($data['cinema_movies'] as $movie) :
-            if (substr($movie->edit_timestamp, 0, 10) ==  date("Y-m-d") || substr($movie->add_timestamp, 0, 10) ==  date("Y-m-d")  ){
-                ?>
-                <tr>
-                    <td>
-                        <img src="<?php echo $movie->img; ?>" alt="cinema img">
-                    </td>
-                    <td>
-                        <h1><?php echo $movie->title_pl; ?></h1>
-                        <h5><?php echo $movie->title_original; ?></h5>
-                        <p><?php foreach (json_decode($movie->info) as $value) {
-                                echo $value; ?> <br />
-                            <?php }
-                            ?></p>
-                        <p style="text-align: justify">Opis filmu: <a href="<?php echo $movie->trailer; ?>" class="trailer">zobacz zwiastun</a> <br />
-                            <?php echo $movie->description; ?></p>
-                    </td>
-                    <td class="seance-hour">
-                        <p><span class="seance-hour"">Godziny seansu:</span>
-                            <?php foreach (json_decode($movie->screen_time) as $value) {
-                                if(strlen($value) != 5 ){
-                                    $data = $value; ?>
-                                   <span><?php echo "<br /><br />" . $value . "<br />";?></span>
+    foreach ($data['cinema_movies'] as $movie) :
+        if (substr($movie->edit_timestamp, 0, 10) ==  date("Y-m-d") || substr($movie->add_timestamp, 0, 10) ==  date("Y-m-d")  ){
+            ?>
+            <tr>
+                <td>
+                    <img src="<?php echo $movie->img; ?>" alt="cinema img">
+                </td>
+                <td>
+                    <h1><?php echo $movie->title_pl; ?></h1>
+                    <h5><?php echo $movie->title_original; ?></h5>
+                    <p><?php foreach (json_decode($movie->info) as $value) {
+                            echo $value; ?> <br />
+                        <?php }
+                        ?></p>
+                    <p style="text-align: justify">Opis filmu: <a href="<?php echo $movie->trailer; ?>" class="trailer">zobacz zwiastun</a> <br />
+                        <?php echo $movie->description; ?></p>
+                </td>
+                <td class="seance-hour">
+                    <p><span class="seance-hour">Godziny seansu:</span>
+                        <?php
+                        $day = 0;
+                        foreach (json_decode($movie->screen_time) as $value) {
+                            if(strlen($value) != 5 ){
+                                $data = $value;
+                                $day++;
+                                ?>
+                                <span><?php echo "<br /><br />" . $value . "<br />";?></span>
                                 <?php
+                            } else {
+                                if($day == 1 && strtotime($value) <= strtotime('+30 minutes' , strtotime(date('H:i')))){
+                                    echo $value.' ';
                                 } else {
-                                    if (strtotime('+30 minutes' , strtotime(date('H:i'))) <= strtotime($value)){
-                                        $seance=str_replace( ' ', '^',$movie->title_original.';'.$movie->title_pl.';'.$data.';'.$value);
-                                        ?>
-                                        <a href="<?php echo URLROOT . '/reservation?seance='.$seance; ?>"><?php echo $value; ?></a>
-                                        <?php
-                                    } else {
-                                        echo $value.' ';
-                                    }
+                                    $seance=str_replace( ' ', '^',$movie->title_original.';'.$movie->title_pl.';'.$data.';'.$value);
+                                    ?>
+                                    <a href="<?php echo URLROOT . '/reservation?seance='.$seance; ?>"><?php echo $value; ?></a>
+                                    <?php
                                 }
                             }
-                            ?></p>
+                        }
+                        ?></p>
 
-                    </td>
-                </tr>
-                <?php
-            }
-        endforeach ?>
-        </tbody>
-    </table>
-
-
+                </td>
+            </tr>
+            <?php
+        }
+    endforeach ?>
+    </tbody>
+</table>
 
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
